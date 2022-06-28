a5be86117125f7a64b890f797468f15bd6f88ecc
lusqua <lucasaguiar.floripa@gmail.com>
2022-06-28 18:46:14 -0300

    FIX. Change name to application

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from datetime import datetime
 from flask import Flask, Response, render_template, stream_with_context
 from flask_sqlalchemy import SQLAlchemy
 
-application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste.db'
-application. config["CACHE_TYPE"] = "null"
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste.db'
+app. config["CACHE_TYPE"] = "null"
 
 template_dir = os.path.abspath('./templates')
 static_dir = os.path.abspath('./static')
 
-db = SQLAlchemy(application)
+db = SQLAlchemy(app)
 
 random.seed()  # Initialize the random number generator
 
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('./dashboard/index.html', data = {'title': 'Início', 'teste': [{'time': datetime.fromtimestamp(953852555).strftime('%Y-%m-%d %H:%M:%S'), 'value': 20}, {'time': datetime.fromtimestamp(1816129816).strftime('%Y-%m-%d %H:%M:%S'), 'value': 12}, {'time': datetime.fromtimestamp(1200396315).strftime('%Y-%m-%d %H:%M:%S'), 'value': 13}, {'time': datetime.fromtimestamp(1180529430).strftime('%Y-%m-%d %H:%M:%S'), 'value': 14}, {'time': datetime.fromtimestamp(1252327542).strftime('%Y-%m-%d %H:%M:%S'), 'value': 17}]})
