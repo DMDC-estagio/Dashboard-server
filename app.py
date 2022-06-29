64217634768dcfa1d44fff85fc230f4165b3ae00
lusqua <lucasaguiar.floripa@gmail.com>
2022-06-29 14:48:39 -0300

    FEAT. Chart-data route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 import os
 from datetime import datetime
 
-from flask import Flask, Response, render_template, stream_with_context
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,6 +16,12 @@ db = SQLAlchemy(app)
 
 random.seed()  # Initialize the random number generator
 
+data = [{'time': datetime.fromtimestamp(953852555).strftime('%Y-%m-%d %H:%M:%S'), 'value': 20}, {'time': datetime.fromtimestamp(1816129816).strftime('%Y-%m-%d %H:%M:%S'), 'value': 12}, {'time': datetime.fromtimestamp(1200396315).strftime('%Y-%m-%d %H:%M:%S'), 'value': 13}, {'time': datetime.fromtimestamp(1180529430).strftime('%Y-%m-%d %H:%M:%S'), 'value': 14}, {'time': datetime.fromtimestamp(1252327542).strftime('%Y-%m-%d %H:%M:%S'), 'value': 17}]
+
 @app.route('/')
 def index():
-    return render_template('./dashboard/index.html', data = {'title': 'Início', 'teste': [{'time': datetime.fromtimestamp(953852555).strftime('%Y-%m-%d %H:%M:%S'), 'value': 20}, {'time': datetime.fromtimestamp(1816129816).strftime('%Y-%m-%d %H:%M:%S'), 'value': 12}, {'time': datetime.fromtimestamp(1200396315).strftime('%Y-%m-%d %H:%M:%S'), 'value': 13}, {'time': datetime.fromtimestamp(1180529430).strftime('%Y-%m-%d %H:%M:%S'), 'value': 14}, {'time': datetime.fromtimestamp(1252327542).strftime('%Y-%m-%d %H:%M:%S'), 'value': 17}]})
+    return render_template('./dashboard/index.html', data = {'title': 'Início', 'teste': data})
+
+@app.route('/chart_data')
+def chart_data():
+    return jsonify(data)
