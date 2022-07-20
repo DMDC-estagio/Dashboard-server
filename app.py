c2a862a25447aad312cf4bddbd58fbddd32a8aae
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-20 17:18:33 -0300

    FEAT. Add Measure module and route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 import os
 from datetime import datetime
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,18 +14,58 @@ static_dir = os.path.abspath('./static')
 
 db = SQLAlchemy(app)
 
-random.seed()  # Initialize the random number generator
+class Measure(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    voltageMed = db.Column(db.Float)
+    currentMed = db.Column(db.Float)
+    voltageMAx = db.Column(db.Float)
+    currentMAx = db.Column(db.Float)
+    voltageMin = db.Column(db.Float)
+    currentMin = db.Column(db.Float)
 
-data = [{'time': datetime.fromtimestamp(953852555).strftime('%Y-%m-%d %H:%M:%S'), 'value': 20}, {'time': datetime.fromtimestamp(1816129816).strftime('%Y-%m-%d %H:%M:%S'), 'value': 12}, {'time': datetime.fromtimestamp(1200396315).strftime('%Y-%m-%d %H:%M:%S'), 'value': 13}, {'time': datetime.fromtimestamp(1180529430).strftime('%Y-%m-%d %H:%M:%S'), 'value': 14}, {'time': datetime.fromtimestamp(1252327542).strftime('%Y-%m-%d %H:%M:%S'), 'value': 17}]
+random.seed()  # Initialize the random number generator
 
 @app.route('/')
 def index():
-    return render_template('./dashboard/index.html', data = {'title': 'Início', 'teste': data})
+    measure_list = Measure.query.all()
+    response = []
+    max_voltage, max_current = 0, 0
+    med_voltage, med_current = 0, 0
+    for item in measure_list:
+        response.append({'time': item.date.strftime('%Y-%m-%d %H:%M:%S'), 'med': item.voltageMed, 'min': item.voltageMin, 'max': item.voltageMAx})
+        if item.voltageMAx > max_voltage:
+            max_voltage = item.voltageMAx
+        if max_current < item.currentMAx:
+            max_current = item.currentMAx
+        med_voltage += item.voltageMed
+        med_current += item.currentMed
+    med_voltage /= len(measure_list)
+    med_current /= len(measure_list)
+
+    return render_template('./dashboard/index.html', data = {'title': 'Início', 'tensao': response, 'tensao_max': f"{round(max_voltage,1)}".split('.'), 'corrente_max': f"{round(max_current,1)}".split('.'), 'tensao_med' : f"{round(med_voltage,1)}".split("."), "corrente_med" : f"{round(med_current,1)}".split(".")})
+
+@app.route('/chart_data/', methods=['POST'])
+def chart_data():
+    new_mesure = Measure(voltageMed=random.randint(0, 100), currentMed=random.randint(0, 100), voltageMAx=random.randint(0, 100), currentMAx=random.randint(0, 100), voltageMin=random.randint(0, 100), currentMin=random.randint(0, 100))
+    db.session.add(new_mesure)
+    db.session.commit()
+    return jsonify(request.get_json())
 
 @app.route('/chart_data/voltage')
 def chart_data_voltage():
-    return jsonify([{'time': datetime.fromtimestamp(953852555).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1816129816).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1200396315).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1180529430).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1252327542).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}])
+    measure_list = Measure.query.all()
+    response = []
+    for item in measure_list:
+        response.append({'time': item.date.strftime('%Y-%m-%d %H:%M:%S'), 'med': item.voltageMed, 'min': item.voltageMin, 'max': item.voltageMAx})
+
+    return jsonify(response)
 
 @app.route('/chart_data/amperage')
 def chart_data_amperage():
-    return jsonify([{'time': datetime.fromtimestamp(953852555).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1816129816).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1200396315).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1180529430).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}, {'time': datetime.fromtimestamp(1252327542).strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(0,200)}])
+    measure_list = Measure.query.all()
+    response = []
+    for item in measure_list:
+        response.append({'time': item.date.strftime('%Y-%m-%d %H:%M:%S'), 'med': item.currentMed, 'min': item.currentMin, 'max': item.currentaMAx})
+
+    return jsonify(response)
