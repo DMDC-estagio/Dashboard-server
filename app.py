00789e6174d5eb2cc5720969d510c8c7cae3870e
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-21 03:17:31 -0300

    Fix. datetime timezone and sort logs

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ def index():
 def logs():
     measure_list = Measure.query.all()
     response = [x.__dict__ for x in measure_list]
+    try: response.sort(key=lambda x: x['date'].timestamp(), reverse=True)
+    except: pass
     for item in response: item['date'] = item['date'].replace(tzinfo=timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
     return render_template('./dashboard/logs.html', data = {'title': 'Logs', 'logs': response})
 
@@ -65,7 +67,7 @@ def group():
 @app.route('/chart_data/', methods=['POST'])
 def chart_data():
     body = dict(request.get_json())
-    new_mesure = Measure(voltageMed=body['voltage']['med'], currentMed=body['current']['med'], voltageMAx=body['voltage']['max'], currentMAx=body['current']['max'], voltageMin=body['voltage']['min'], currentMin=body['current']['min'])
+    new_mesure = Measure(date=datetime.now(timezone('America/Sao_Paulo')),voltageMed=body['voltage']['med'], currentMed=body['current']['med'], voltageMAx=body['voltage']['max'], currentMAx=body['current']['max'], voltageMin=body['voltage']['min'], currentMin=body['current']['min'])
     try:
         db.session.add(new_mesure)
         db.session.commit()
