303983faa641cc5d94b63188fc4985d908d5dd6c
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-22 19:39:19 -0300

    Fix. Beatifull responses in case of error

--- app/routes/chart_data.py
+++ app/routes/chart_data.py
@@ -11,10 +11,25 @@ chartRouter = Blueprint('chartData', __name__)
 def chart_data():
     body = dict(request.get_json())
     try:
+        verifyParams(body)
+
         new_mesure = Measure(date=datetime.now(timezone('America/Sao_Paulo')),voltageMed=body['voltage']['med'], currentMed=body['current']['med'], voltageMAx=body['voltage']['max'], currentMAx=body['current']['max'], voltageMin=body['voltage']['min'], currentMin=body['current']['min'])
+
         db.session.add(new_mesure)
         db.session.commit()
     except Exception as e:
         db.session.rollback()
-        return jsonify({'Status': 'Maybe something is missing','error': str(e)}), 500
-    return jsonify({'success': 'ok'}), 201
+        return jsonify({'Status': 'BAD REQUEST','error': str(e)}), 400
+
+    return jsonify({'Status': 'OK', 'Status': 'Measurement created with sucessfull'}), 201
+
+def verifyParams(body):
+    if 'voltage' not in body or 'current' not in body:
+        raise Exception('Missing parameters voltage or current in body')
+    if 'med' not in body['voltage'] or 'max' not in body['voltage'] or 'min' not in body['voltage']:
+        raise Exception('Missing voltage parameters in body')
+    if 'med' not in body['current'] or 'max' not in body['current'] or 'min' not in body['current']:
+        raise Exception('Missing current parameters in body')
+    if (type(body['voltage']['med']) != float and type(body['voltage']['med']) != int) or (type(body['voltage']['max']) != float and type(body['voltage']['max']) != int) or (type(body['voltage']['min']) != float and type(body['voltage']['min']) != int):
+        raise Exception('Invalid type of parameters in body, need float or int')
+    return True
