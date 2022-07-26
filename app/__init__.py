13c4a3182621ff4c0cfe0bd7ce9e37f069246a5e
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-26 11:08:47 -0300

    Feat. Added dotenv to run local

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+from dotenv import load_dotenv
 import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config["CACHE_TYPE"] = "null"
