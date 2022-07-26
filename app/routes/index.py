d2be27c72ab25ac1b5744974a5b4b861b7a8ec97
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-26 18:01:57 -0300

    Fix. Re-structured index route function

--- app/routes/index.py
+++ app/routes/index.py
@@ -6,26 +6,31 @@ indexRouter = Blueprint('index', __name__)
 
 @indexRouter.route('/')
 def index():
-    measure_list = Measure.query.all()
-    response = []
-    max_voltage, max_current = 0, 0
-    med_voltage, med_current = 0, 0
-    for item in measure_list:
-        response.append({'time': item.date.replace(tzinfo=timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S'), 'med': item.voltageMed, 'min': item.voltageMin, 'max': item.voltageMAx})
-        if item.voltageMAx > max_voltage:
-            max_voltage = item.voltageMAx
-        if max_current < item.currentMAx:
-            max_current = item.currentMAx
-        med_voltage += item.voltageMed
-        med_current += item.currentMed
-        # print(item.date.replace(tzinfo=timezone('America/Sao_Paulo')), item.date)
-    try:
-        med_voltage /= len(measure_list)
-        med_current /= len(measure_list)
-    except:
-        med_voltage = 0
-        med_current = 0
+    measure_list = [x.__dict__ for x in Measure.query.order_by(Measure.date.asc()).limit(15)]
+    max_voltage, max_current = getMax(measure_list, 'voltageMAx'), getMax(measure_list, 'currentMAx')
+    med_voltage, med_current = getMean(measure_list, 'voltageMed'), getMean(measure_list, 'currentMed')
 
-    data = {'title': 'Início', 'tensao': response, 'tensao_max': f"{round(max_voltage,1)}".split('.'), 'corrente_max': f"{round(max_current,1)}".split('.'), 'tensao_med' : f"{round(med_voltage,1)}".split("."), "corrente_med" : f"{round(med_current,1)}".split(".")}
+    response = [{
+        'time': x['date'].replace(tzinfo=timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S'),
+        'med': x['voltageMed'],
+        'min': x['voltageMin'],
+        'max': x['voltageMAx']
+    } for x in measure_list]
+
+    data = {
+        'title': 'Início',
+        'tensao': response,
+        'tensao_max': f'{round(max_voltage,1)}'.split('.'),
+        'corrente_max': f'{round(max_current,1)}'.split('.'),
+        'tensao_med' : f'{round(med_voltage,1)}'.split('.'),
+        'corrente_med' : f'{round(med_current,1)}'.split('.')
+    }
 
     return render_template('./dashboard/index.html', data = data)
+
+
+def getMax(list, key):
+    return max(list, key=lambda x: x['voltageMAx'])[key]
+
+def getMean(list, key):
+    return sum([x[key] for x in list]) / len(list)
