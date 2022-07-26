cb4afdb79ab4c6c26ce8f55320b0c2cfa64a0d14
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-26 18:13:18 -0300

    Fix. Re-structure paginate function

--- app/helpers/paginate.py
+++ app/helpers/paginate.py
@@ -2,8 +2,14 @@ from flask import request
 
 def paginate(query, per_page = 15):
     page = request.args.get('page', default = 1, type = int)
+
+    total_pages = getTotalPages(total, per_page)
     total = query.count()
+    items = query.paginate(page=page, per_page=per_page).items
+
+    return {'total': total, 'page': page ,'total_pages': total_pages, 'items': items}
+
+def getTotalPages(total, per_page = 15):
     total_pages = total//per_page
     total_pages += 1 if total%per_page != 0 else 0
-    items = query.paginate(page=page, per_page=per_page).items
-    return {'total': total, 'page': page ,'total_pages': total_pages, 'items': items}
+    return total_pages
