7a1625c66acd1884fbb5dfdcbb2c607458d32be2
lusqua <lucasaguiar.floripa@gmail.com>
2022-07-26 18:32:58 -0300

    Fix. variable 'total' referenced before assignment

--- app/helpers/paginate.py
+++ app/helpers/paginate.py
@@ -3,8 +3,8 @@ from flask import request
 def paginate(query, per_page = 15):
     page = request.args.get('page', default = 1, type = int)
 
-    total_pages = getTotalPages(total, per_page)
     total = query.count()
+    total_pages = getTotalPages(total, per_page)
     items = query.paginate(page=page, per_page=per_page).items
 
     return {'total': total, 'page': page ,'total_pages': total_pages, 'items': items}
